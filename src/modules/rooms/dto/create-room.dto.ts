6d462a33d5b8c2ad4e4af97470cb26b949c93adb
CuongNvcqht3012002 <nguyencuong3012002@gmail.com>
2024-08-16 13:42:26 +0700

    fix: room module

--- src/modules/rooms/dto/create-room.dto.ts
+++ src/modules/rooms/dto/create-room.dto.ts
@@ -5,6 +5,6 @@ import { IsNotEmpty } from 'class-validator'
 export class CreateRoomDto {
   @ApiProperty({ example: 'Phòng 1 Tầng 4' })
   @Transform(({ value }) => value?.trim())
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Tên phòng không được để trống' })
   name: string
 }
